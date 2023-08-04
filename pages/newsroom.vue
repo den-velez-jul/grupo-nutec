ae1d92c95bfa29ff4b15bed348bc43b991afb84f
den <dennis.velez@julius2grow.com>
2023-08-04 11:33:52 -0600

    fix order news at newsroom page

--- pages/newsroom.vue
+++ pages/newsroom.vue
@@ -4,27 +4,27 @@
     <div
       class="flex flex-col justify-center font-founders-grosteskers text-white z-10">
       <span class="font-bold md:text-[20px] xl:text-[30px]">
-        <PrismicText :field="firstNews.article_top_label" />
+        <PrismicText :field="heroNews.article_top_label" />
       </span>
       <h1
         class="mt-[30px] text-[35px] md:text-[50px] lg:text-[60px] xl:text-[70px]">
-        <PrismicText :field="firstNews.article_title" />
+        <PrismicText :field="heroNews.article_title" />
       </h1>
       <p class="mt-[40px]">
-        <PrismicText :field="firstNews.article_author" />
+        <PrismicText :field="heroNews.article_author" />
       </p>
       <p class="mt-[16px]">
-        {{ firstNews.article_date }}
+        {{ heroNews.article_date }}
       </p>
       <p class="mt-5">
-        <PrismicText :field="firstNews.article_description" />
+        <PrismicText :field="heroNews.article_description" />
       </p>
       <div class="flex justify-start mt-10">
         <PrismicLink
-          :field="firstNews.article_cta_link"
+          :field="heroNews.article_cta_link"
           class="flex items-center text-white">
           <span class="font-semibold md:text-[20px] xl:text-[30px]">
-            {{ firstNews.article_cta_label[0].text }}
+            {{ heroNews.article_cta_label[0].text }}
           </span>
           <img src="~assets/icons/arrow-white.svg" class="ml-3" />
         </PrismicLink>
@@ -39,7 +39,7 @@
   <div
     class="mx-6 md:mx-[50px] lg:mx-[75px] xl:mx-auto max-w-[1920px] xl:px-[100px]">
     <section
-      id="firstNews"
+      id="headlineNews"
       class="pt-[100px] pb-[80px] md:pt-[80px] lg:pt-[150px] lg:pb-[100px]">
       <h4
         class="text-[24px] font-bold text-center font-founders-grosteskers text-dark-blue md:text-[30px] xl:text-[40px]">
@@ -49,28 +49,28 @@
         class="mt-[60px] lg:grid lg:grid-cols-2 lg:mt-[70px] lg:gap-x-[60px]">
         <div class="lg:col-start-2 lg:col-end-3 lg:row-start-1 lg:row-end-2">
           <img
-            :src="firstNews.article_image.url"
+            :src="headlineNews.article_image.url"
             class="h-[300px] md:h-[524px] w-full object-cover" />
         </div>
         <div class="font-founders-grosteskers">
           <h1 class="text-blue mt-8 lg:mt-0 xl:text-[70px]">
-            <PrismicText :field="firstNews.article_title" />
+            <PrismicText :field="headlineNews.article_title" />
           </h1>
           <p class="mt-[30px] md:mt-[50px] lg:mt-[60px] xl:text-[20px]">
-            <PrismicText :field="firstNews.article_author" />
+            <PrismicText :field="headlineNews.article_author" />
           </p>
           <p class="mt-[30px] md:mt-[40px] lg:mt-[30px] xl:text-[20px]">
-            {{ firstNews.article_date }}
+            {{ headlineNews.article_date }}
           </p>
           <p class="mt-[15px] lg:mt-5 xl:text-[20px]">
-            <PrismicText :field="firstNews.article_description" />
+            <PrismicText :field="headlineNews.article_description" />
           </p>
           <div class="flex justify-start mt-[30px] lg:mt-10">
             <PrismicLink
-              :field="firstNews.article_cta_link"
+              :field="headlineNews.article_cta_link"
               class="flex items-center text-dark-blue">
               <span class="font-semibold md:text-[20px] xl:text-[30px]">
-                {{ firstNews.article_cta_label[0].text }}
+                {{ headlineNews.article_cta_label[0].text }}
               </span>
               <img src="~assets/icons/arrow-dark-blue.svg" class="ml-3" />
             </PrismicLink>
@@ -83,25 +83,23 @@
       class="pb-[60px] md:pt-[70px] lg:pt-[100px] lg:pb-[150px]">
       <div
         class="grid grid-cols-1 gap-y-10 md:grid-cols-2 md:gap-x-10 lg:grid-cols-3">
-        <div class=" " v-for="news of regularNews">
+        <div class=" " v-for="news of cardNews">
           <CardArticle
-            :title="news.data.article_title"
-            :image="news.data.article_image"
-            :author="news.data.article_author"
-            :date="news.data.article_date"
-            :ctaLabel="news.data.article_cta_label"
-            :ctaLink="news.data.article_cta_link" />
+            :title="news.article_title"
+            :image="news.article_image"
+            :author="news.article_author"
+            :date="news.article_date"
+            :ctaLabel="news.article_cta_label"
+            :ctaLink="news.article_cta_link" />
         </div>
       </div>
       <div class="flex justify-start mt-[30px] xl:mt-[50px]">
-        <PrismicLink
-          :field="firstNews.article_cta_link"
-          class="flex items-center text-dark-blue">
+        <a class="flex items-center text-dark-blue">
           <span class="font-semibold md:text-[20px] xl:text-[30px]">
             Explora Todos los Articulos y Editoriales
           </span>
           <img src="~assets/icons/arrow-dark-blue.svg" class="ml-3" />
-        </PrismicLink>
+        </a>
       </div>
     </section>
 
@@ -134,7 +132,7 @@ const { data: news } = await useAsyncData("news", () =>
 );
 
 const homeData = home.value.data;
-const newsData = news.value;
+const newsList = news.value;
 
 const events = {
   title: homeData.events_title,
@@ -146,7 +144,20 @@ const events = {
   ctaLink: homeData.events_cta_link,
 };
 
-const heroNews = news.value[0].data;
-const firstNews = news.value[2].data;
-const regularNews = [...news.value.slice(2)];
+let heroNews;
+let headlineNews;
+let cardNews = [];
+
+newsList.forEach((news) => {
+  if (news.tags.length === 0) {
+    cardNews.push(news.data);
+    return;
+  }
+  if (news.tags.includes("Hero")) {
+    heroNews = news.data;
+    return;
+  } else {
+    headlineNews = news.data;
+  }
+});
 </script>
